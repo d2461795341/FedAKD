26a5fd0a938d806e35eeb4356e7bb6e3e98fef85
dkj <2461795341@qq.com>
2023-12-14 21:45:30 +0800

    FedAKD

---
+++ cks/fed_medmnist_myfedmode_20_0.1_0.01_0.001_0.5_1_1.0_0.6_100_5_nosharebn_server_0.03/reveal.py
@@ -0,0 +1,13 @@
+import xlwt
+import xlrd
+import torch
+
+
+
+d1=torch.load('permyfedmode')
+print(d1['best_epoch'])
+print(d1['best_macc'])
+
+
+
+
---
+++ cks/消融实验/agg_lam/fed_pamap_myfedmode_20_0.1_0.01_0.001_0.5_1_1.0_0.6_100_5_server_0.9/reveal.py
@@ -0,0 +1,13 @@
+import xlwt
+import xlrd
+import torch
+
+
+
+d1=torch.load('glomyfedmode')
+
+print(d1['best_macc'])
+
+
+
+
